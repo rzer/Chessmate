1996d20e2038a06973e0cba2a345bccd134651d2
rzer <rzer@mail.ru>
2023-02-04 20:58:16 +0300

    убраны ошибки компиляции шахмотного движка

--- src/bbc.c
+++ src/bbc.c
@@ -80,17 +80,12 @@
  ==================================
 \**********************************/
 
-
+#include "pd.h"
 // system headers
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#ifdef WIN64
-    #include <windows.h>
-#else
-    # include <sys/time.h>
-#endif
+
 
 // define version
 #define version "1.2"
@@ -375,115 +370,9 @@ int stopped = 0;
 // get time in milliseconds
 int get_time_ms()
 {
-    #ifdef WIN64
-        return GetTickCount();
-    #else
-        struct timeval time_value;
-        gettimeofday(&time_value, NULL);
-        return time_value.tv_sec * 1000 + time_value.tv_usec / 1000;
-    #endif
+    return pd->system->getCurrentTimeMilliseconds();
 }
 
-/*
-
-  Function to "listen" to GUI's input during search.
-  It's waiting for the user input from STDIN.
-  OS dependent.
-  
-  First Richard Allbert aka BluefeverSoftware grabbed it from somewhere...
-  And then Code Monkey King has grabbed it from VICE)
-  
-*/
-  
-int input_waiting()
-{
-    #ifndef WIN32
-        fd_set readfds;
-        struct timeval tv;
-        FD_ZERO (&readfds);
-        FD_SET (fileno(stdin), &readfds);
-        tv.tv_sec=0; tv.tv_usec=0;
-        select(16, &readfds, 0, 0, &tv);
-
-        return (FD_ISSET(fileno(stdin), &readfds));
-    #else
-        static int init = 0, pipe;
-        static HANDLE inh;
-        DWORD dw;
-
-        if (!init)
-        {
-            init = 1;
-            inh = GetStdHandle(STD_INPUT_HANDLE);
-            pipe = !GetConsoleMode(inh, &dw);
-            if (!pipe)
-            {
-                SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT|ENABLE_WINDOW_INPUT));
-                FlushConsoleInputBuffer(inh);
-            }
-        }
-        
-        if (pipe)
-        {
-           if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL)) return 1;
-           return dw;
-        }
-        
-        else
-        {
-           GetNumberOfConsoleInputEvents(inh, &dw);
-           return dw <= 1 ? 0 : dw;
-        }
-
-    #endif
-}
-
-// read GUI/user input
-void read_input()
-{
-    // bytes to read holder
-    int bytes;
-    
-    // GUI/user input
-    char input[256] = "", *endc;
-
-    // "listen" to STDIN
-    if (input_waiting())
-    {
-        // tell engine to stop calculating
-        stopped = 1;
-        
-        // loop to read bytes from STDIN
-        do
-        {
-            // read bytes from STDIN
-            bytes=read(fileno(stdin), input, 256);
-        }
-        
-        // until bytes available
-        while (bytes < 0);
-        
-        // searches for the first occurrence of '\n'
-        endc = strchr(input,'\n');
-        
-        // if found new line set value at pointer to 0
-        if (endc) *endc=0;
-        
-        // if input is available
-        if (strlen(input) > 0)
-        {
-            // match UCI "quit" command
-            if (!strncmp(input, "quit", 4))
-                // tell engine to terminate exacution    
-                quit = 1;
-
-            // // match UCI "stop" command
-            else if (!strncmp(input, "stop", 4))
-                // tell engine to terminate exacution
-                quit = 1;
-        }   
-    }
-}
 
 // a bridge function to interact between search and GUI input
 static void communicate() {
@@ -493,8 +382,7 @@ static void communicate() {
 		stopped = 1;
 	}
 	
-    // read GUI input
-	read_input();
+    //TODO здесь организовать взаимодействие с моими классами
 }
 
 
@@ -3737,7 +3625,7 @@ static inline int score_move(int move)
 static inline int sort_moves(moves *move_list)
 {
     // move scores
-    int move_scores[move_list->count];
+    int move_scores[256];
     
     // score all the moves within a move list
     for (int count = 0; count < move_list->count; count++)
